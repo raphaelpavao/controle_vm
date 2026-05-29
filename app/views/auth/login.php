cc0e3eb3d1b01560ad8166b82eacd514a7a9ca99
Raphael Pavao <raphaelpavao@gmail.com>
2026-05-29 16:05:26 -0300

    Teste de publicação automática

--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -4,7 +4,7 @@
             <span class="brand-mark">VM</span>
             <strong><?= e($appName) ?></strong>
         </div>
-        <h1>Acesso ao sistema</h1>
+        <h1>Acesso ao sistema teste de deploy</h1>
 
         <?php if (!empty($errors['login'])): ?>
             <div class="flash error"><?= e($errors['login']) ?></div>
